cca54295ee489a0ac4ecd3810ff3314439b8cb37
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-05-14 22:27:27 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+var mysql = require('mysql');
+const options = {
+	user: 'root',
+	password: 'password',
+	database: 'mydb'
+};
+const connection = mysql.createConnection(options);
+
+const App = () => {
+	const [todo, settodo] = useState([]);
+	const [dummyState, setDummyState] = useState('initialState');
+
+	useEffect(() => {
+		const fetchData = async () => {
+			let data = await fetch('https://jsonplaceholder.typicode.com/todos/2');
+			data = await data.json();
+			settodo([...todo, data]);
+		};
+		fetchData();
+		connection.query('SELECT * FROM user', (error, todos, fields) => {
+			if (error) {
+				console.error('An error occurred while executing the query');
+				throw error;
+			}
+			console.log(todos);
+		});
+	}, []);
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>{dummyState}</p>
+				{todo.map(x => {
+					return <p>{x.title}</p>;
+				})}
+				<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
+};
 
 export default App;
