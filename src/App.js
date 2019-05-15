db8af3c6799f76b7111b455af21d0707f1126d7f
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-05-14 23:40:29 -0400

    delete mysql connection from main app

--- src/App.js
+++ src/App.js
@@ -2,41 +2,26 @@ import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-var mysql = require('mysql');
-const options = {
-	user: 'root',
-	password: 'password',
-	database: 'mydb'
-};
-const connection = mysql.createConnection(options);
-
 const App = () => {
-	const [todo, settodo] = useState([]);
-	const [dummyState, setDummyState] = useState('initialState');
+	const [users, setusers] = useState([]);
 
 	useEffect(() => {
 		const fetchData = async () => {
-			let data = await fetch('https://jsonplaceholder.typicode.com/todos/2');
+			let data = await fetch('http://localhost:3000/users/');
 			data = await data.json();
-			settodo([...todo, data]);
+			console.log('fetched data', data);
+			setusers([users, ...data]);
+			console.log(users);
 		};
 		fetchData();
-		connection.query('SELECT * FROM user', (error, todos, fields) => {
-			if (error) {
-				console.error('An error occurred while executing the query');
-				throw error;
-			}
-			console.log(todos);
-		});
 	}, []);
 
 	return (
 		<div className='App'>
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
-				<p>{dummyState}</p>
-				{todo.map(x => {
-					return <p>{x.title}</p>;
+				{users.map((x, i) => {
+					return <p key={i}>{x.email}</p>;
 				})}
 				<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
 					Learn React
