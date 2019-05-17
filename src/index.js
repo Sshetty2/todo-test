9ada53d51016c0d9d9d0bc274608419c4f8cf610
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-05-17 08:38:51 -0400

    add redux provider

--- src/index.js
+++ src/index.js
@@ -4,7 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/index';
+
+const devTools = composeWithDevTools(applyMiddleware(thunk));
+const store = createStore(rootReducer, devTools);
+const provider = (
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
+
+ReactDOM.render(provider, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
