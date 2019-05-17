c976caac7605cd19aead9297fa29043f0dd7245f
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-05-17 08:39:36 -0400

    update users route

--- backend-test/routes/users.js
+++ backend-test/routes/users.js
@@ -17,4 +17,29 @@ router.get('/', function(req, res, next) {
 	});
 });
 
+router.post('/', function(req, res, next) {
+	const options = {
+		host: 'localhost',
+		port: '3306',
+		user: 'root',
+		password: 'password',
+		database: 'mydb'
+	};
+	const connection = mysql.createConnection(options);
+	console.log(req.body);
+	const sqlString = `INSERT INTO user
+		(username,
+		email,
+		password)
+		VALUES
+		('${req.body.userid}',
+		'${req.body.email}',
+		'${req.body.pw_hash}')`;
+	console.log(sqlString);
+	connection.query(sqlString, result => {
+		console.log(result);
+	});
+	res('we good');
+});
+
 module.exports = router;
